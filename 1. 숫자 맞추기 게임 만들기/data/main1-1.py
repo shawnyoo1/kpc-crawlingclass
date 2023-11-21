1b04d9a689c1f606832996199a1187932e0b37cd
shawnyoo1 <sunghanyoo@gmail.com>
2023-11-21 10:06:24 +0900

    guess number game complete

--- 1. 숫자 맞추기 게임 만들기/data/main1-1.py
+++ 1. 숫자 맞추기 게임 만들기/data/main1-1.py
@@ -1,18 +1,12 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import random
 class RandomNumber:
     def get_random_numbers__(self):
         random_number = random.randint(a:1, b:100)
         return random_number
 
+    def __init__(self):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     r = RandomNumber()
     random_number = r.get_random_numbers__()
     print(random_number)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
